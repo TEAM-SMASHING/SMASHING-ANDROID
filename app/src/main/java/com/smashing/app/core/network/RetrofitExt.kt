d416abcab132a0bd6dd5a5127660e81a6b6532ac
seungjun <jjw5741@naver.com>
2025-12-30 19:50:52 +0900

    [add/#1] network 관련 설정 추가

---
+++ app/src/main/java/com/smashing/app/core/network/RetrofitExt.kt
@@ -0,0 +1,5 @@
+package com.smashing.app.core.network
+
+fun String?.isJsonObject(): Boolean = this?.startsWith("{") == true && this.endsWith("}")
+
+fun String?.isJsonArray(): Boolean = this?.startsWith("[") == true && this.endsWith("]")
