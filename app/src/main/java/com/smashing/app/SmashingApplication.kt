2d7545827a046752cb32ca8e2dd3da98cf1ea204
seungjun <jjw5741@naver.com>
2025-12-31 18:13:50 +0900

    [add/#1] hilt 기초 세팅 추가

---
+++ app/src/main/java/com/smashing/app/SmashingApplication.kt
@@ -0,0 +1,24 @@
+package com.smashing.app
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class SmashingApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initTimber()
+        setDayMode()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+    private fun setDayMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
