bd58764ce4959923cb0a6a3dd9cf645e9a4308c2
SHC <hcshin0717@naver.com>
2026-01-05 17:43:12 +0900

    [feat/#5] 지역 탐색 Screen 구현 및 navigation Route 생성

---
+++ app/src/main/java/com/smashing/app/presentation/region/RegionScreen.kt
@@ -0,0 +1,180 @@
+package com.smashing.app.presentation.region
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.smashing.app.core.common.state.UiState
+import com.smashing.app.core.extension.noRippleClickable
+import com.smashing.app.data.model.Region
+import kotlinx.collections.immutable.ImmutableList
+
+@Composable
+fun RegionRoute(
+    modifier: Modifier = Modifier,
+    viewModel: RegionViewModel = hiltViewModel(),
+) {
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    RegionScreen(
+        uiState = uiState,
+        onSearchQueryChange = viewModel::updateSearchQuery,
+        onSearch = viewModel::fetchRegion,
+        onRegionSelected = viewModel::getRegion,
+        modifier = modifier,
+    )
+}
+
+@Composable
+private fun RegionScreen(
+    uiState: RegionContract.State,
+    onSearchQueryChange: (String) -> Unit,
+    onSearch: (String) -> Unit,
+    onRegionSelected: (Region) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+    ) {
+        Text(
+            text = "활동지역검색",
+        )
+
+        OutlinedTextField(
+            value = uiState.searchQuery,
+            onValueChange = { query ->
+                onSearchQueryChange(query)
+                if (query.isNotBlank()) {
+                    onSearch(query)
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            placeholder = {
+                Text(text = "주소를 입력하세요")
+            },
+        )
+        uiState.selectedRegion?.let { selected ->
+            Spacer(Modifier.height(8.dp))
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+            ) {
+                Text(
+                    text = "선택된 지역: ${selected.region2depthName}",
+                    modifier = Modifier.padding(16.dp),
+                )
+            }
+        }
+        Spacer(Modifier.height(8.dp))
+        when (val loadState = uiState.regionLoadState) {
+            is UiState.Idle -> {
+
+            }
+
+            is UiState.Loading -> {
+                Text(
+                    text = "검색 중...",
+                    modifier = Modifier.padding(16.dp),
+                )
+            }
+
+            is UiState.Failure -> {
+                Text(
+                    text = "오류: ${loadState.msg}",
+                    modifier = Modifier.padding(16.dp),
+                )
+            }
+
+            is UiState.Success -> {
+                RegionList(
+                    regions = loadState.data,
+                    onRegionClick = onRegionSelected,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun RegionList(
+    regions: ImmutableList<Region>,
+    onRegionClick: (Region) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(
+            items = regions,
+        ) { region ->
+            RegionItem(
+                region = region,
+                onClick = { onRegionClick(region) },
+            )
+        }
+    }
+}
+
+@Composable
+private fun RegionItem(
+    region: Region,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .noRippleClickable(onClick = onClick),
+    ) {
+        Column {
+            Text(
+                text = region.addressName,
+                modifier = Modifier.padding(16.dp),
+            )
+            Text(
+                text = region.region2depthName,
+                modifier = Modifier.padding(16.dp),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RegionScreenPreview() {
+    RegionScreen(
+        uiState = RegionContract.State(),
+        onSearchQueryChange = {},
+        onSearch = {},
+        onRegionSelected = {},
+    )
+}
