3adcd7ac19efa66d0d247a84912c583e328dd9bc
oilbeaneda <20221444@sungshin.ac.kr>
2026-01-06 22:49:57 +0900

    [MOD#12] 코리 반영

---
+++ app/src/main/java/com/smashing/app/core/common/type/ChipType.kt
@@ -0,0 +1,28 @@
+package com.smashing.app.core.common.type
+
+import androidx.compose.ui.graphics.Color
+
+
+//TODO 디자인 시스템 등록 후 수정 현재는 임시로 값 할당함
+enum class ChipType(
+    val backgroundColor: Color,
+    val contentColor: Color,
+    val borderColor: Color? = null,
+) {
+    ACTIVE(
+        backgroundColor = Color.White,
+        contentColor = Color.Black,
+        borderColor = Color(0xFFE2E6EA),
+    ),
+    INACTIVE(
+        backgroundColor = Color.Transparent,
+        contentColor = Color.White,
+        borderColor = Color(0xFF252A36),
+    ),
+    DISABLED(
+        backgroundColor = Color(0xFF252A36),
+        contentColor = Color.White,
+        borderColor = Color(0xFF252A36),
+    )
+
+}
--- app/src/main/java/com/smashing/app/core/designsystem/component/chip/SmashingChip.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/chip/SmashingChip.kt
@@ -1,6 +1,8 @@
 package com.smashing.app.core.designsystem.component.chip
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,12 +11,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -22,30 +23,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.smashing.app.R.drawable.ic_fake_red
+import com.smashing.app.core.common.type.ChipType
 
-
-//TODO 디자인 시스템 등록 후 수정 현재는 임시로 값 할당함
-enum class ChipState(
-    val backgroundColor: Color,
-    val contentColor: Color,
-    val borderColor: Color? = null,
-) {
-    Active(
-        backgroundColor = Color.White,
-        contentColor = Color.Black,
-        borderColor = Color(0xFFE2E6EA)
-    ),
-    Inactive(
-        backgroundColor = Color.Transparent,
-        contentColor = Color.White,
-        borderColor = Color(0xFF252A36)
-    ),
-    Disabled(
-        backgroundColor = Color(0xFF252A36),
-        contentColor = Color.White
-    )
-
-}
 
 /**
  *Smashing Chip
@@ -55,54 +34,62 @@ enum class ChipState(
  *
  * @param text Chip에 표시될 텍스트
  * @param state Chip의 상태 (Active, Inactive, Disabled)
- * @param onClick 칩 클릭 시 실행될 콜백 함수
  * @param icon 표시할 아이콘(선택사항)
+ * @param onClick 칩 클릭 시 실행될 콜백 함수
+ *
  *
  */
 
 @Composable
 fun SmashingChip(
     text: String,
-    state: ChipState,
-    modifier: Modifier = Modifier,
-    icon: ImageVector? = null,
-    iconContentDescription: String? = null,
-    onClick: () -> Unit
+    state: ChipType,
+    onClick: () -> Unit,
+    icon: ImageVector,
+    iconContentDescription: String,
+    modifier: Modifier = Modifier
 ) {
-    Surface(
-        onClick = onClick,
-        enabled = state != ChipState.Disabled,
-        shape = RoundedCornerShape(12.dp),
-        color = state.backgroundColor,
-        border = state.borderColor?.let { BorderStroke(1.dp, it) },
-        contentColor = state.contentColor,
+    Row(
         modifier = modifier
-    ) {
-        Row(
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 10.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-            if (icon != null) {
-                Icon(
-                    imageVector = icon,
-                    contentDescription = iconContentDescription,
-                    tint = state.contentColor,
-                    modifier = Modifier.size(24.dp)
-                )
-                Spacer(modifier = Modifier.width(10.dp))
-            }
-            Text(
-                text = text,
-                style = androidx.compose.ui.text.TextStyle(
-                    fontSize = 14.sp,
-                    lineHeight = 21.sp,
-                    fontWeight = FontWeight(400),
-                    color = state.contentColor
-                )
+            .clip(RoundedCornerShape(12.dp))
+            .background(state.backgroundColor)
+            .then(
+                if (state.borderColor != null) {
+                    Modifier.border(
+                        width = 1.dp,
+                        color = state.borderColor,
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                } else {
+                    Modifier
+                }
             )
+            .clickable(
+                onClick = onClick,
+                enabled = state != ChipType.DISABLED
+            )
+            .padding(horizontal = 16.dp, vertical = 10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = iconContentDescription,
+            tint = state.contentColor,
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
 
-        }
+        Text(
+            text = text,
+            style = androidx.compose.ui.text.TextStyle(
+                fontSize = 14.sp,
+                lineHeight = 21.sp,
+                fontWeight = FontWeight(400),
+                color = state.contentColor
+            )
+        )
+
     }
 }
 
@@ -116,7 +103,7 @@ private fun PreviewSmashingChips() {
     ) {
         SmashingChip(
             text = "text",
-            state = ChipState.Active,
+            state = ChipType.ACTIVE,
             icon = ImageVector.vectorResource(id = ic_fake_red),
             iconContentDescription = "icon",
             onClick = {}
@@ -124,7 +111,7 @@ private fun PreviewSmashingChips() {
 
         SmashingChip(
             text = "text",
-            state = ChipState.Inactive,
+            state = ChipType.INACTIVE,
             icon = ImageVector.vectorResource(id = ic_fake_red),
             iconContentDescription = "icon",
             onClick = {}
@@ -132,7 +119,7 @@ private fun PreviewSmashingChips() {
 
         SmashingChip(
             text = "text",
-            state = ChipState.Disabled,
+            state = ChipType.DISABLED,
             icon = ImageVector.vectorResource(id = ic_fake_red),
             iconContentDescription = "icon",
             onClick = {}
