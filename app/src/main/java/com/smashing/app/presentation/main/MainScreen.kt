78ec8789b2fab85ddfe04021c265887c3234e94f
seungjun <jjw5741@naver.com>
2026-01-08 11:02:53 +0900

    [feat/#18] main screen navigation 처리

--- app/src/main/java/com/smashing/app/presentation/main/MainScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/main/MainScreen.kt
@@ -3,17 +3,23 @@ package com.smashing.app.presentation.main
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.exclude
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
-import com.smashing.app.presentation.dummy.dummyGraph
+import com.smashing.app.core.designsystem.theme.SmashingTheme
 import com.smashing.app.presentation.home.navigation.homeGraph
 import com.smashing.app.presentation.main.component.MainBottomBar
+import com.smashing.app.presentation.matching.navigation.matchingGraph
+import com.smashing.app.presentation.profile.navigation.profileGraph
+import com.smashing.app.presentation.search.navigation.searchGraph
 import kotlinx.collections.immutable.toImmutableList
 
 @Composable
@@ -32,7 +38,8 @@ fun MainScreen(
                 onTabSelected = appState::navigate,
             )
         },
-        containerColor = Color.White,
+        containerColor = SmashingTheme.colors.bgCanvas,
+        contentWindowInsets = WindowInsets.safeDrawing.exclude(WindowInsets.statusBars),
         modifier = Modifier
             .fillMaxSize(),
     ) { innerPadding ->
@@ -60,7 +67,15 @@ private fun MainNavHost(
             innerPadding = innerPadding,
         )
 
-        dummyGraph(
+        searchGraph(
+            innerPadding = innerPadding,
+        )
+
+        matchingGraph(
+            innerPadding = innerPadding,
+        )
+
+        profileGraph(
             innerPadding = innerPadding,
         )
     }
