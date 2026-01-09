87147afb8cf132c3bf7e8060eaa3dab7838e2b80
seungjun <jjw5741@naver.com>
2026-01-10 00:37:19 +0900

    [feat/#34] 임시 프로필 이미지 util 함수 구현

---
+++ app/src/main/java/com/smashing/app/core/util/ProfileImageProvider.kt
@@ -0,0 +1,23 @@
+package com.smashing.app.core.util
+
+import kotlin.math.absoluteValue
+
+object ProfileImageProvider {
+    private val TEMP_PROFILE_IMAGE_URLS = listOf(
+        "https://picsum.photos/100",
+        "https://picsum.photos/100",
+        "https://picsum.photos/100",
+        "https://picsum.photos/100",
+    )
+
+    /**
+     * userId를 기반으로 고정된 임시 URL을 반환합니다.
+     * @param userId 사용자 식별자 (null일 경우 기본 이미지 반환)
+     */
+    fun getTempUrl(userId: String?): String {
+        if (userId.isNullOrBlank()) return TEMP_PROFILE_IMAGE_URLS.first()
+
+        val index = hashCode().absoluteValue % TEMP_PROFILE_IMAGE_URLS.size
+        return TEMP_PROFILE_IMAGE_URLS[index]
+    }
+}
