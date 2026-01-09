c1a989a01eb9466c35f76eb0a4acf29cbcf725e9
oilbeaneda <20221444@sungshin.ac.kr>
2026-01-10 03:52:44 +0900

    [ui#31]SmashingSearchTextField

--- app/src/main/java/com/smashing/app/core/designsystem/component/textfield/SmashingSearchTextField.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/textfield/SmashingSearchTextField.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -27,6 +28,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.smashing.app.R
+import com.smashing.app.core.common.style.ColoredBoxTextFieldStyle
 import com.smashing.app.core.designsystem.theme.SmashingAndroidTheme
 import com.smashing.app.core.designsystem.theme.SmashingTheme
 import com.smashing.app.core.extension.noRippleClickable
@@ -34,67 +36,87 @@ import com.smashing.app.core.extension.noRippleClickable
 /**
  *  검색 텍스트 필드 공통 컴포넌트입니다.
  * @param state 텍스트 필드의 상태 (입력값 및 커서 위치 관리)
- * @param placeholder 입력값이 없을 때 표시되는 힌트 텍스트
  * @param onSearch 키보드의 검색(돋보기) 버튼 클릭 시 호출되는 콜백 (입력된 텍스트 반환)
+ * @param placeholder 입력값이 없을 때 표시되는 힌트 텍스트
+ *
  */
 
 @Composable
 fun SearchTextField(
     state: TextFieldState,
+    onBackClick: () -> Unit,
     onSearch: (String) -> Unit,
     modifier: Modifier = Modifier,
     placeholder: String = "",
 ) {
     val interactionSource = remember { MutableInteractionSource() }
     val isFocused by interactionSource.collectIsFocusedAsState()
-
     val isFilled by remember { derivedStateOf { state.text.isNotEmpty() } }
     val focusManager = LocalFocusManager.current
-    val backgroundColor = SmashingTheme.colors.bgSurface
-
+    val inputState = ColoredBoxTextFieldStyle.from(
+        isFocused,
+        isFilled,
+    )
     Row(
-        modifier = modifier
-            .background(
-                color = backgroundColor,
-                shape = RoundedCornerShape(8.dp),
-            )
-            .padding(vertical = 13.dp,horizontal = 16.dp),
+        modifier = modifier,
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        if (!isFilled) {
-            Icon(
-                imageVector = ImageVector.vectorResource(R.drawable.ic_search_sm),
-                contentDescription = null,
-                tint = SmashingTheme.colors.iconSecondary,
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_arrow_left),
+            contentDescription = "뒤로가기",
+            tint = SmashingTheme.colors.iconPrimary,
+            modifier = Modifier
+                .noRippleClickable(onClick = onBackClick),
+
+            )
+        Spacer(modifier = Modifier.padding(start = 8.dp))
+
+        Row(
+            modifier = Modifier
+                .background(
+                    color = inputState.getBackgroundColor(),
+                    shape = RoundedCornerShape(8.dp),
+                )
+                .padding(vertical = 13.dp, horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+
+            if (!isFilled) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_search_sm),
+                    contentDescription = "돋보기",
+                    tint = SmashingTheme.colors.iconSecondary,
+                )
+            }
+            Spacer(modifier = Modifier.padding(start = 4.dp))
+
+            SmashingBasicTextField(
+                state = state,
+                modifier = Modifier.weight(1f),
+                placeholder = placeholder,
+                placeholderColor = inputState.getContentColor(),
+                placeholderStyle = inputState.getTextStyle(),
+                textColor = inputState.getContentColor(),
+                textStyle = inputState.getTextStyle(),
+                interactionSource = interactionSource,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                onKeyboardAction = {
+                    focusManager.clearFocus()
+                    onSearch(state.text.toString())
+                },
+                suffix = {
+                    if (isFocused && isFilled) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.ic_circle_x),
+                            contentDescription = "삭제",
+                            tint = Color.Unspecified,
+                            modifier = Modifier
+                                .noRippleClickable(onClick = state::clearText),
+                        )
+                    }
+                }
             )
         }
-
-        SmashingBasicTextField(
-            state = state,
-            modifier = Modifier.weight(1f),
-            placeholder = placeholder,
-            placeholderColor = SmashingTheme.colors.txtDisabled,
-            placeholderStyle = SmashingTheme.typography.sm.medium14,
-            textColor = SmashingTheme.colors.txtPrimary,
-            textStyle = SmashingTheme.typography.sm.medium14,
-            interactionSource = interactionSource,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-            onKeyboardAction = {
-                focusManager.clearFocus()
-                onSearch(state.text.toString())
-            },
-            suffix = {
-                if (isFocused && isFilled) {
-                    Icon(
-                        imageVector = ImageVector.vectorResource(R.drawable.ic_circle_x),
-                        contentDescription = "삭제",
-                        tint = Color.Unspecified,
-                        modifier = Modifier
-                            .noRippleClickable(onClick = state::clearText),
-                    )
-                }
-            }
-        )
     }
 }
 
@@ -108,6 +130,8 @@ private fun SearchTextFieldStatesPreview() {
                 state = remember { TextFieldState() },
                 placeholder = "닉네임을 입력해주세요",
                 modifier = Modifier.fillMaxWidth(),
+                onBackClick = {//실제 뒤로가기 로직 수행
+                },
                 onSearch = { query ->
                     // 실제 검색 로직 수행 (예: ViewModel 호출)
                     println("검색어: $query")
