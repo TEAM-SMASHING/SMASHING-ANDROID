684ef7d250ce8ba662441f839a29d55abe66dfbf
seungjun <jjw5741@naver.com>
2026-01-10 02:32:26 +0900

    [refactor/#34] tier 스케일 대응

--- app/src/main/java/com/smashing/app/core/designsystem/component/badge/TierBadge.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/badge/TierBadge.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.smashing.app.core.common.type.TierType
@@ -50,6 +51,8 @@ fun TierBadge(
         style = typography.xs.regular12,
         color = textColor,
         textAlign = TextAlign.Center,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
         modifier = modifier
             .width(67.dp)
             .background(
