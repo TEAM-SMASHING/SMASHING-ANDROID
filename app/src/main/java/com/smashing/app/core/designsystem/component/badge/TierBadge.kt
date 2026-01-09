fcc85d8ffc9e862dc658b408c0169e511fb352c1
seungjun <jjw5741@naver.com>
2026-01-10 02:21:35 +0900

    [refactor/#34] badge ReadOnly Composable 추가

--- app/src/main/java/com/smashing/app/core/designsystem/component/badge/TierBadge.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/badge/TierBadge.kt
@@ -1,4 +1,4 @@
-package com.smashing.app.core.common.badge
+package com.smashing.app.core.designsystem.component.badge
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -59,6 +60,7 @@ fun TierBadge(
     )
 }
 
+@ReadOnlyComposable
 @Composable
 private fun getTierBadgeColors(
     tierType: TierType,
