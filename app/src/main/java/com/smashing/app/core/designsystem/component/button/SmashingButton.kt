973d8b1fc0c2d188b08f2f70cfa09d5e02859fec
seungjun <jjw5741@naver.com>
2026-01-10 01:54:57 +0900

    [refactor/#34] ReadOnly Composable 추가

--- app/src/main/java/com/smashing/app/core/designsystem/component/button/SmashingButton.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/button/SmashingButton.kt
@@ -21,7 +21,7 @@ import com.smashing.app.core.designsystem.theme.SmashingTheme
 
 /**
  * 앱의 기본 버튼 컴포넌트
- * @param buttonType 버튼 색상 타입
+ * @param buttonStyle 버튼 색상 타입
  * @param text 버튼 텍스트
  * @param onClick 클릭 시 실행될 콜백
  * @param isEnabled 활성화 상태
