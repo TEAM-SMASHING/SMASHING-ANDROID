45005c739fe140065d8ddd8b871a58c7d71dbd21
oilbeaneda <20221444@sungshin.ac.kr>
2026-01-12 00:12:35 +0900

    [ui/#47]TextAreaTextField 컴포넌트 수정

--- app/src/main/java/com/smashing/app/core/designsystem/component/textfield/SmashingAreaTextField.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/textfield/SmashingAreaTextField.kt
@@ -13,12 +13,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.InputTransformation
-import androidx.compose.foundation.text.input.TextFieldState
-import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.maxLength
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
@@ -36,12 +39,13 @@ fun SmashingAreaTextField(
     placeholder: String,
     modifier: Modifier = Modifier,
     isError: Boolean = false,
-    inputTransformation: InputTransformation? = null,
+    maxLength: Int = 100,
     keyboardOptions: KeyboardOptions = KeyboardOptions(imeAction = ImeAction.Default),
 ) {
     val interactionSource = remember { MutableInteractionSource() }
     val isFocused by interactionSource.collectIsFocusedAsState()
     val focusManager = LocalFocusManager.current
+    val currentLength = state.text.length
     val isFilled = state.text.isNotEmpty()
     val inputState = BorderInputStyle.from(
         isFocused = isFocused,
@@ -66,16 +70,22 @@ fun SmashingAreaTextField(
             placeholderStyle = inputState.getTextStyle(),
             textColor = inputState.getContentColor(),
             textStyle = inputState.getTextStyle(),
-            inputTransformation = inputTransformation,
-            interactionSource = interactionSource,
+            inputTransformation = InputTransformation.maxLength(maxLength),
             keyboardOptions = keyboardOptions,
             lineLimits = TextFieldLineLimits.MultiLine(),
             modifier = Modifier
                 .matchParentSize(),
-            onKeyboardAction = {focusManager.clearFocus()},
+            onKeyboardAction = { focusManager.clearFocus() },
+        )
+        Text(
+            text = "$currentLength / $maxLength",
+            style = SmashingTheme.typography.xs.regular12,
+            color = inputState.getContentColor(),
+            modifier = Modifier.align(Alignment.BottomEnd)
         )
     }
 }
+
 
 @Preview
 @Composable
