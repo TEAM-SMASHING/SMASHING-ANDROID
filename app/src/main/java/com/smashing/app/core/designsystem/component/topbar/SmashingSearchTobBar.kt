548ef36e11704fc590f892a8052399cb498ab3d9
SHC <hcshin0717@naver.com>
2026-01-13 03:11:38 +0900

    [ui/#61] TopBarType 기반 검색 탑바 구현

---
+++ app/src/main/java/com/smashing/app/core/designsystem/component/topbar/SmashingSearchTobBar.kt
@@ -0,0 +1,74 @@
+package com.smashing.app.core.designsystem.component.topbar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.smashing.app.R.drawable.ic_arrow_left
+import com.smashing.app.core.designsystem.component.textfield.SearchTextField
+import com.smashing.app.core.designsystem.theme.SmashingAndroidTheme
+import com.smashing.app.core.designsystem.theme.SmashingTheme
+import com.smashing.app.core.extension.noRippleClickable
+
+@Composable
+fun SmashingSearchTobBar(
+    searchState: TextFieldState,
+    placeholder: String,
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .background(
+                color = Color.Transparent,
+            )
+            .padding(
+                vertical = 10.dp,
+                horizontal = 16.dp,
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(ic_arrow_left),
+            contentDescription = null,
+            tint = SmashingTheme.colors.iconPrimary,
+            modifier = Modifier
+                .noRippleClickable(
+                    onClick = onBackClick
+                ),
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        SearchTextField(
+            state = searchState,
+            placeholder = placeholder,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+private fun SmashingSearchTobBarPreview() {
+    SmashingAndroidTheme {
+        SmashingSearchTobBar(
+            searchState = rememberTextFieldState(),
+            placeholder = "닉네임을 입력해주세요",
+            onBackClick = {},
+        )
+    }
+}
