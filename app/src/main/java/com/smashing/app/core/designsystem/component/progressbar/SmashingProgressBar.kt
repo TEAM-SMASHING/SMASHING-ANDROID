3d978a9342754f19d162b44f33cb608a9a37bd9c
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-01-13 01:29:11 +0900

    [mod/#54] 코리 반영
    - 상수화
    - tc
    - stringResource 추출

--- app/src/main/java/com/smashing/app/core/designsystem/component/progressbar/SmashingProgressBar.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/progressbar/SmashingProgressBar.kt
@@ -22,8 +22,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.smashing.app.R.string.progress_label
 import com.smashing.app.core.designsystem.theme.SmashingAndroidTheme
 import com.smashing.app.core.designsystem.theme.SmashingTheme.colors
 
@@ -33,6 +35,7 @@ import com.smashing.app.core.designsystem.theme.SmashingTheme.colors
  * @param progress  프로그래스바 진행률 (최소 0.0, 최대 1.0)
  */
 
+private const val durationMillis = 500
 
 @Composable
 fun SmashingProgressBar(
@@ -42,10 +45,10 @@ fun SmashingProgressBar(
     val animatedProgress by animateFloatAsState(
         targetValue = progress,
         animationSpec = tween(
-            durationMillis = 500,
-            easing = FastOutSlowInEasing
+            durationMillis = durationMillis,
+            easing = FastOutSlowInEasing,
         ),
-        label = "progress",
+        label = stringResource(progress_label),
     )
 
     Box(
