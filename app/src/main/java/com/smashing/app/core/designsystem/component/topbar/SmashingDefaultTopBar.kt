94285320155917187517e50dfc020aa2bd9b99ed
SHC <hcshin0717@naver.com>
2026-01-13 03:36:38 +0900

    [add/#61] tc 추가

--- app/src/main/java/com/smashing/app/core/designsystem/component/topbar/SmashingDefaultTopBar.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/topbar/SmashingDefaultTopBar.kt
@@ -43,7 +43,7 @@ fun SmashingDefaultTopBar(
                     .align(Alignment.CenterStart)
                     .padding(start = 16.dp)
                     .noRippleClickable(
-                        onClick = onClick
+                        onClick = onClick,
                     ),
             )
         }
