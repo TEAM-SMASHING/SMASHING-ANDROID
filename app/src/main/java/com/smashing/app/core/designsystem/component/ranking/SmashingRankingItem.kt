d10743563ed691239f3848e14543fa718cf00b75
SHC <hcshin0717@naver.com>
2026-01-12 14:48:29 +0900

    [del/#46] 사용하지 않는 import 제거

--- app/src/main/java/com/smashing/app/core/designsystem/component/ranking/SmashingRankingItem.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/ranking/SmashingRankingItem.kt
@@ -1,6 +1,5 @@
 package com.smashing.app.core.designsystem.component.ranking
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
