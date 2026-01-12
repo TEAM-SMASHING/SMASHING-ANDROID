a0fdd3590fb05d5e7868e7039cb8900c1eafba43
seungjun <jjw5741@naver.com>
2026-01-12 18:05:02 +0900

    [add/#44] Matching model 추가

---
+++ app/src/main/java/com/smashing/app/data/model/matching/AcceptedMatching.kt
@@ -0,0 +1,17 @@
+package com.smashing.app.data.model.matching
+
+import com.smashing.app.core.common.type.GenderType
+import com.smashing.app.core.common.type.TierType
+import java.time.OffsetDateTime
+
+data class AcceptedMatching(
+    val matchingId: String,
+    val gameId: String,
+    val userId: String,
+    val nickname: String,
+    val genderType: GenderType,
+    val tierType: TierType,
+    val openChatUrl: String,
+    val isResultBannerBlocked: Boolean,
+    val cooldownUntil: OffsetDateTime,
+)
