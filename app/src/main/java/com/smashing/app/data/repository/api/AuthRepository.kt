99873f77603084521bccf6dd88bea6c85e28bbee
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-01-12 17:09:30 +0900

    [add/#52] AuthRepository 추가

--- app/src/main/java/com/smashing/app/data/repository/api/AuthRepository.kt
+++ app/src/main/java/com/smashing/app/data/repository/api/AuthRepository.kt
@@ -2,8 +2,11 @@ package com.smashing.app.data.repository.api
 
 import android.content.Context
 import com.smashing.app.data.model.auth.KakaoLoginToken
+import com.smashing.app.data.model.auth.SignUpModel
+import com.smashing.app.data.remote.dto.PostSignUpRequest
 
 interface AuthRepository {
     suspend fun loginKakao(context: Context): Result<String>
     suspend fun postKakaoLogin(authorization: String): Result<KakaoLoginToken>
+    suspend fun postSignUp(request: PostSignUpRequest): Result<SignUpModel>
 }
--- app/src/main/java/com/smashing/app/data/repository/impl/AuthRepositoryImpl.kt
+++ app/src/main/java/com/smashing/app/data/repository/impl/AuthRepositoryImpl.kt
@@ -3,9 +3,12 @@ package com.smashing.app.data.repository.impl
 import android.content.Context
 import com.smashing.app.core.util.suspendRunCatching
 import com.smashing.app.data.mapper.toKakaoLoginToken
+import com.smashing.app.data.mapper.toSignUpModel
 import com.smashing.app.data.model.auth.KakaoLoginToken
+import com.smashing.app.data.model.auth.SignUpModel
 import com.smashing.app.data.remote.datasource.api.AuthRemoteDataSource
 import com.smashing.app.data.remote.datasource.api.KakaoAuthDataSource
+import com.smashing.app.data.remote.dto.PostSignUpRequest
 import com.smashing.app.data.repository.api.AuthRepository
 import javax.inject.Inject
 
@@ -21,6 +24,13 @@ class AuthRepositoryImpl @Inject constructor(
         suspendRunCatching {
             val response = authRemoteDataSource.postKakaoLogin(authorization)
             response.data?.toKakaoLoginToken()
+                ?: throw IllegalArgumentException("response data is null")
+        }
+
+    override suspend fun postSignUp(request: PostSignUpRequest): Result<SignUpModel> =
+        suspendRunCatching {
+            val response = authRemoteDataSource.postSignUp(request)
+            response.data?.toSignUpModel()
                 ?: throw IllegalArgumentException("response data is null")
         }
 }
