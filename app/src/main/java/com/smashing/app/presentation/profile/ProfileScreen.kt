c21548119d48fcab8910df96ff97ce10364c4915
oilbeaneda <20221444@sungshin.ac.kr>
2026-01-13 13:37:27 +0900

    [ui/#51]프로필 뷰 UI

--- app/src/main/java/com/smashing/app/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/profile/ProfileScreen.kt
@@ -1,49 +1,170 @@
 package com.smashing.app.presentation.profile
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.smashing.app.R
+import com.smashing.app.R.drawable.ic_fake_red
+import com.smashing.app.core.common.type.SportType
+import com.smashing.app.core.common.type.TierType
 import com.smashing.app.core.designsystem.theme.SmashingAndroidTheme
+import com.smashing.app.core.designsystem.theme.SmashingTheme
+import com.smashing.app.data.model.ProfileReview
+import com.smashing.app.data.model.UserProfileInfo
+import com.smashing.app.presentation.profile.component.ProfileReviewCard
+import com.smashing.app.presentation.profile.component.ProfileStatsBar
+import com.smashing.app.presentation.profile.component.ProfileTierBox
+import kotlinx.collections.immutable.toImmutableList
+
 
 @Composable
 fun ProfileRoute(
     modifier: Modifier = Modifier,
     viewModel: ProfileViewModel = hiltViewModel(),
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     ProfileScreen(
         modifier = modifier,
+        uiState = uiState,
+        onSportClick = {},
+        onAddSportClick = { },
+        onTierGuideClick = {},
+        onReviewsClick = {},
     )
 }
 
 @Composable
 private fun ProfileScreen(
+    uiState: ProfileContract.State,
+    onAddSportClick: () -> Unit,
+    onTierGuideClick: () -> Unit,
+    onReviewsClick: () -> Unit,
+    onSportClick: (SportType) -> Unit,
     modifier: Modifier = Modifier,
+    lazyListState: LazyListState = rememberLazyListState(),
 ) {
-    Column(
+    val navigationBarBottomPadding =
+        WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+    val info = uiState.profileInfo
+
+    if (info == null) {
+        Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text("loding")
+        }
+        return
+    }
+
+    LazyColumn(
+        state = lazyListState,
         modifier = modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+            .fillMaxSize()
+            .background(color = SmashingTheme.colors.bgCanvas)
+            .statusBarsPadding()
+            .padding(horizontal = 16.dp),
+        contentPadding = PaddingValues(bottom = 20.dp + navigationBarBottomPadding),
     ) {
-        Text(
-            text = "프로필",
-            color = Color.White,
-        )
+        item {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 21.dp, horizontal = 16.dp),
+            ) {
+                Text(
+                    text = stringResource(R.string.profile),
+                    color = SmashingTheme.colors.txtPrimary,
+                    style = SmashingTheme.typography.md.semibold16,
+                    modifier = Modifier.align(Alignment.Center),
+                )
+            }
+        }
+        item {
+            ProfileTierBox(
+                tierType = uiState.profileInfo.tierType,
+                sports = uiState.profileInfo.mySports,
+
+                selectedSport = uiState.profileInfo.selectedSport,
+                onSportClick = onSportClick,
+
+                tierIconResId = uiState.profileInfo.tierIconResId,
+                progress = uiState.profileInfo.lpProgress,
+                lpStatus = uiState.profileInfo.minLp,
+                totalLp = uiState.profileInfo.maxLp,
+                onTierInfoClick = onTierGuideClick,
+                onAddSportClick = onAddSportClick,
+            )
+        }
+        item {
+            ProfileStatsBar(
+                winCount = uiState.profileInfo.winCount,
+                loseCount = uiState.profileInfo.loseCount,
+            )
+        }
+        item {
+            Box(modifier = Modifier.padding(16.dp)) {
+                ProfileReviewCard(
+                    reviews = uiState.reviews,
+                    onViewAllClick = onReviewsClick,
+                )
+            }
+        }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 private fun ProfileScreenPreview() {
+    val dummyReviews = listOf(
+        ProfileReview(1, "닝우닝", "2일 전", "매너도 좋고, 너무 잘하세요!"),
+        ProfileReview(2, "닝우닝닝이", "4일 전", "매너도 좋고, 너무 잘하세요!"),
+        ProfileReview(3, "닝우", "5일 전", "매너도 좋고, 너무 잘하세요! 매너도 좋고, 너무 잘하세요! 매너도 좋고, 너무 잘하세요!")
+    ).toImmutableList()
+
+    val dummyState = ProfileContract.State(
+        profileInfo = UserProfileInfo(
+            tierType = TierType.GOLD_1,
+            tierIconResId = ic_fake_red,
+            mySports = listOf(SportType.PING_PONG, SportType.BADMINTON),
+            selectedSport = SportType.PING_PONG,
+            lpProgress = 0.1f,
+            minLp  = 100,
+            maxLp = 500,
+            winCount = 4,
+            loseCount = 5,
+        ),
+        reviews= dummyReviews
+    )
+
+
     SmashingAndroidTheme {
-        ProfileScreen()
+        ProfileScreen(
+            uiState = dummyState,
+            onAddSportClick = {},
+            onTierGuideClick = {},
+            onReviewsClick = {},
+            onSportClick = {},
+        )
     }
 }
+
--- app/src/main/java/com/smashing/app/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/smashing/app/presentation/profile/ProfileViewModel.kt
@@ -1,11 +1,93 @@
 package com.smashing.app.presentation.profile
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.smashing.app.R
+import com.smashing.app.core.common.type.SportType
+import com.smashing.app.core.common.type.TierType
+import com.smashing.app.data.model.ProfileReview
+import com.smashing.app.data.model.UserProfileInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
 ) : ViewModel() {
 
+    private val _uiState = MutableStateFlow(ProfileContract.State())
+    val uiState: StateFlow<ProfileContract.State> = _uiState.asStateFlow()
+
+    init {
+        fetchProfileData()
+    }
+
+    // TODO 더미 데이터 삭제 예정
+    private fun fetchProfileData() {
+        viewModelScope.launch {
+            _uiState.update { it.copy(loadState = ProfileUiState.Loading) }
+
+            try {
+                // TODO: 실제 API 호출 (delay로 시뮬레이션)
+                delay(1000)
+
+                val dummyProfile = UserProfileInfo(
+                    tierType = TierType.GOLD_1,
+                    tierIconResId = R.drawable.ic_fake_red,
+                    mySports = listOf(),
+                    selectedSport = SportType.BADMINTON,
+                    lpProgress = 0.2f,
+                    minLp = 100,
+                    maxLp = 500,
+                    winCount = 12,
+                    loseCount = 5,
+                )
+
+                val dummyReviews = persistentListOf(
+                    ProfileReview(1, "닝우닝", "2일 전", "매너 굿!"),
+                    ProfileReview(2, "스매싱", "4일 전", "너무 잘해요"),
+                )
+
+                _uiState.update {
+                    it.copy(
+                        loadState = ProfileUiState.Success,
+                        profileInfo = dummyProfile,
+                        reviews = dummyReviews,
+                    )
+                }
+
+            } catch (e: Exception) {
+                _uiState.update {
+                    it.copy(loadState = ProfileUiState.Failure(e.message ?: "Unknown Error"))
+                }
+            }
+        }
+    }
+
+    fun updateSelectedSport(sport: SportType) {
+        val currentInfo = _uiState.value.profileInfo ?: return
+
+        if (currentInfo.selectedSport == sport) return
+
+        // TODO: 실제로는 여기서 종목에 맞는 전적 데이터 서버 호출
+        val newTier = if (sport == SportType.PING_PONG) TierType.GOLD_1 else TierType.SILVER_2
+        val newWinCount = if (sport == SportType.PING_PONG) 12 else 5
+
+        _uiState.update { currentState ->
+            currentState.copy(
+                profileInfo = currentInfo.copy(
+                    selectedSport = sport,
+                    tierType = newTier,
+                    winCount = newWinCount,
+                    //나머지 필드들도 필요시 업데이트
+                )
+            )
+        }
+    }
 }
