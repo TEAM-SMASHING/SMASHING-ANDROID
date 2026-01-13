b505936580aac5338da623f3e75479a810811ebc
seungjun <jjw5741@naver.com>
2026-01-14 01:36:03 +0900

    [fix/#62] Winner drop down padding 수정

--- app/src/main/java/com/smashing/app/core/designsystem/component/dropdown/DropDownItem.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/dropdown/DropDownItem.kt
@@ -1,5 +1,7 @@
 package com.smashing.app.core.designsystem.component.dropdown
 
+import androidx.compose.runtime.Stable
+
 /**
  * @param label 드롭다운 메뉴의 레이블
  *
@@ -11,6 +13,7 @@ package com.smashing.app.core.designsystem.component.dropdown
  * 추가 액션을 가진 드롭다운 메뉴 항목
  * 선택 시 별도의 onClick 콜백을 실행합니다.
  */
+@Stable
 sealed interface DropdownItem {
     val label: String
     data class Normal(override val label: String) : DropdownItem
--- app/src/main/java/com/smashing/app/core/designsystem/component/dropdown/SmashingWinnerDropdown.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/dropdown/SmashingWinnerDropdown.kt
@@ -3,8 +3,11 @@ package com.smashing.app.core.designsystem.component.dropdown
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,25 +18,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.smashing.app.R.drawable.ic_arrow_down
-import com.smashing.app.core.extension.noRippleClickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.smashing.app.R.string.dropdown_winner_select
 import com.smashing.app.core.designsystem.theme.SmashingAndroidTheme
 import com.smashing.app.core.designsystem.theme.SmashingTheme.colors
 import com.smashing.app.core.designsystem.theme.SmashingTheme.typography
+import com.smashing.app.core.extension.noRippleClickable
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
@@ -48,6 +47,8 @@ import kotlinx.collections.immutable.toImmutableList
  * @param placeholder 선택된 항목이 없을 때 표시할 안내 문구 (기본값: "승자 선택")
  * @param isDivide 항목 사이에 구분선을 표시할지 여부 (기본값: true)
  */
+private const val MENU_WEIGHT = 30 / 22f
+
 @Composable
 fun SmashingWinnerDropdown(
     selectedItem: String?,
@@ -63,20 +64,21 @@ fun SmashingWinnerDropdown(
     var triggerHeight by remember { mutableStateOf(0.dp) }
 
     val density = LocalDensity.current
+    val menuTextColor = if (selectedItem == null) colors.txtDisabled else colors.txtSecondary
 
     Box(
         modifier = modifier,
     ) {
         Row(
             modifier = Modifier
+                .width(140.dp)
                 .onGloballyPositioned { coordinates ->
                     triggerWidth = with(density) { coordinates.size.width.toDp() }
                     triggerHeight = with(density) { coordinates.size.height.toDp() }
                 }
-                .defaultMinSize(minWidth = 123.dp)
                 .background(
                     color = colors.bgSurface,
-                    shape = RoundedCornerShape(8.dp)
+                    shape = RoundedCornerShape(8.dp),
                 )
                 .noRippleClickable(
                     onClick = {
@@ -84,30 +86,30 @@ fun SmashingWinnerDropdown(
                             isExpanded = !isExpanded
                         }
                     }
-                )
-                .padding(vertical = 8.dp)
-                .padding(start = 16.dp, end = 8.dp),
+                ),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center,
         ) {
+            if (selectedItem == null) Spacer(Modifier.weight(MENU_WEIGHT))
+
             Text(
                 text = selectedItem ?: placeholder,
                 style = typography.sm.medium14,
-                color = if (selectedItem == null) {
-                    colors.txtDisabled
-                } else {
-                    colors.txtSecondary
-                },
+                color = menuTextColor,
+                modifier = Modifier
+                    .padding(vertical = 12.dp),
             )
-
-            Spacer(modifier = Modifier.width(8.dp))
 
             if (selectedItem == null) {
                 Icon(
                     imageVector = ImageVector.vectorResource(ic_arrow_down),
                     contentDescription = null,
                     tint = colors.txtDisabled,
+                    modifier = Modifier
+                        .padding(start = 12.dp),
                 )
+
+                Spacer(Modifier.weight(1f))
             }
         }
 
