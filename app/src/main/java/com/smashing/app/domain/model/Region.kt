f2275d857fba261becbf3641f05792a44592997b
SHC <hcshin0717@naver.com>
2026-01-16 02:39:03 +0900

    [chore/#76] Region 어노테이션 삭제

--- app/src/main/java/com/smashing/app/domain/model/Region.kt
+++ app/src/main/java/com/smashing/app/domain/model/Region.kt
@@ -1,14 +1,10 @@
 package com.smashing.app.domain.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class Region(
     val addressName: String,
     val cityName: String,
     val districtName: String,
-) : Parcelable {
+){
     val isSeoulDistrict: Boolean
         get() = cityName == "서울" && districtName.isNotBlank()
 }
