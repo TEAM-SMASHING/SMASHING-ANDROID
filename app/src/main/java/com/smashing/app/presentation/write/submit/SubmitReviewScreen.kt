9bdb8835f0131079f05e6c828374cc90b0b20f30
seungjun <jjw5741@naver.com>
2026-01-16 04:37:58 +0900

    [chore/#70] dialog 텍스트 수정

--- app/src/main/java/com/smashing/app/presentation/write/submit/SubmitReviewScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/write/submit/SubmitReviewScreen.kt
@@ -129,7 +129,7 @@ private fun SubmitReviewScreen(
         if (isConfirmDialogOpen) {
             SmashingDialog(
                 title = "매칭 결과를 제출하시겠습니까?",
-                subtitle = "정확한 경기 결과각 아닐 경우 반려될 수 있어요.",
+                subtitle = "정확한 경기 결과가 아닐 경우 반려될 수 있어요.",
                 type = DialogStyle.CONFIRM,
                 confirmText = "확인",
                 onDismissRequest = { isConfirmDialogOpen = false },
