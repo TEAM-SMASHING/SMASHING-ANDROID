034baa8930e88d8df896639c7654d72882544b7b
SHC <hcshin0717@naver.com>
2026-01-15 23:28:56 +0900

    [add/#76] 지역 변경 Contract 생성

---
+++ app/src/main/java/com/smashing/app/presentation/home/regionchange/RegionChangeContract.kt
@@ -0,0 +1,22 @@
+package com.smashing.app.presentation.home.regionchange
+
+import androidx.compose.runtime.Immutable
+import com.smashing.app.domain.model.Region
+
+interface RegionChangeContract {
+    @Immutable
+    data class State(
+        val selectedRegion: Region? = null,
+        val regionLoadState: RegionChangeUiState = RegionChangeUiState.Idle,
+    )
+}
+
+sealed interface RegionChangeUiState{
+    object Idle : RegionChangeUiState
+
+    object Success : RegionChangeUiState
+
+    data class Failure(
+        val msg: String,
+    ) : RegionChangeUiState
+}
