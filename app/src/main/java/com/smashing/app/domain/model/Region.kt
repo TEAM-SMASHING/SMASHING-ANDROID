5d07fea68c995e5fe49122d73ac67ee76a35bc19
SHC <hcshin0717@naver.com>
2026-01-16 01:06:39 +0900

    [chore/#76] Region Parcelize 어노테이션 수정

--- app/src/main/java/com/smashing/app/domain/model/Region.kt
+++ app/src/main/java/com/smashing/app/domain/model/Region.kt
@@ -1,13 +1,14 @@
 package com.smashing.app.domain.model
 
-import kotlinx.serialization.Serializable
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-@Serializable
+@Parcelize
 data class Region(
     val addressName: String,
     val cityName: String,
     val districtName: String,
-) {
+) : Parcelable {
     val isSeoulDistrict: Boolean
         get() = cityName == "서울" && districtName.isNotBlank()
 }
