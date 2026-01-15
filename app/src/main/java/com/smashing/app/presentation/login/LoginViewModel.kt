0e18f67c6cef394b81439d42f9083e98196b1141
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-01-15 16:02:11 +0900

    [mod/#63] signup 네비게이션 sideEffect로 수정

--- app/src/main/java/com/smashing/app/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/smashing/app/presentation/login/LoginViewModel.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.smashing.app.data.repository.api.AuthRepository
+import com.smashing.app.presentation.login.LoginContract.SideEffect.NavigateToSignUp
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -14,19 +17,20 @@ class LoginViewModel @Inject constructor(
     private val authRepository: AuthRepository,
 ) : ViewModel() {
 
+    private val _sideEffect = MutableSharedFlow<LoginContract.SideEffect>()
+    val sideEffect = _sideEffect.asSharedFlow()
+
     fun postKakaoLogin(
         context: Context,
-        onSignedUpUserLoginSuccess: () -> Unit,
-        onNewUserLoginSuccess: (String) -> Unit,
     ) = viewModelScope.launch {
         authRepository.loginKakao(context = context)
             .onSuccess { token ->
                 authRepository.postKakaoLogin(token)
                     .onSuccess {
                         if (it.isCompletedSignUp) {
-                            onSignedUpUserLoginSuccess()
+                            _sideEffect.emit(LoginContract.SideEffect.NavigateToHome)
                         } else {
-                            onNewUserLoginSuccess(it.kakaoId)
+                            _sideEffect.emit(NavigateToSignUp(it.kakaoId))
                         }
                         Timber.tag("KakaoLogin").d("로그인 성공 $token")
                     }
