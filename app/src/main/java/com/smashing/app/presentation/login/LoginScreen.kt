7d6ee604a282b4569ae356888cce8ca02d626c7d
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-01-16 03:47:25 +0900

    [hotfix/#80] 함수 호출 수정

--- app/src/main/java/com/smashing/app/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/login/LoginScreen.kt
@@ -56,8 +56,8 @@ fun LoginRoute(
         viewModel.sideEffect.flowWithLifecycle(lifecycle = lifecycleOwner.lifecycle)
             .collect { sideEffect ->
                 when (sideEffect) {
-                    is NavigateToHome -> navigateToHome
-                    is NavigateToSignUp -> navigateToSignUp
+                    is NavigateToHome -> navigateToHome()
+                    is NavigateToSignUp -> navigateToSignUp(sideEffect.kakaoId)
                 }
             }
     }
