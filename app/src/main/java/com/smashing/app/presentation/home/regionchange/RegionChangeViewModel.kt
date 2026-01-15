96c230b68b95355892edb31c1784f7d5409d9bc3
SHC <hcshin0717@naver.com>
2026-01-15 23:29:13 +0900

    [feat/#76] 지역 변경 ViewModel 생성

---
+++ app/src/main/java/com/smashing/app/presentation/home/regionchange/RegionChangeViewModel.kt
@@ -0,0 +1,14 @@
+package com.smashing.app.presentation.home.regionchange
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class RegionChangeViewModel @Inject constructor() : ViewModel() {
+    private val _uiState = MutableStateFlow(RegionChangeContract.State())
+    val uiState = _uiState.asStateFlow()
+
+}
