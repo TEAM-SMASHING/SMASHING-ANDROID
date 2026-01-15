60de959a13eca35ad16d89912d8dbd596224ea14
seungjun <jjw5741@naver.com>
2026-01-15 13:29:40 +0900

    [add/#70] confirm 화면 세팅

--- app/src/main/java/com/smashing/app/presentation/write/confirm/ConfirmScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/write/confirm/ConfirmScreen.kt
@@ -1,1 +1,31 @@
 package com.smashing.app.presentation.write.confirm
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.smashing.app.core.designsystem.theme.SmashingAndroidTheme
+
+@Composable
+fun ConfirmRoute(
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+
+}
+
+@Composable
+private fun ConfirmScreen(
+    modifier: Modifier = Modifier,
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ConfirmScreenPreview() {
+    SmashingAndroidTheme {
+        ConfirmScreen(
+
+        )
+    }
+}
