2876127b379c759e6790c12973fc983020fd714e
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-01-16 03:54:48 +0900

    [hotfix/#80] 온클릭 삭제

--- app/src/main/java/com/smashing/app/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/login/LoginScreen.kt
@@ -94,10 +94,7 @@ private fun LoginScreen(
             contentDescription = null,
             modifier = Modifier
                 .width(240.dp)
-                .aspectRatio(LOGO_RATIO)
-                .noRippleClickable(
-                    onClick = { onKakaoLoginClick }
-                ),
+                .aspectRatio(LOGO_RATIO),
         )
 
         Spacer(modifier = Modifier.height(8.dp))
