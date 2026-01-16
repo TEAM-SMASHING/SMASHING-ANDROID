1349edb611cc2bc34d943a18eda3da7e11f7746f
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-01-16 23:46:23 +0900

    [feat/#82] 채팅 링크 service 추가

--- app/src/main/java/com/smashing/app/data/remote/service/AuthService.kt
+++ app/src/main/java/com/smashing/app/data/remote/service/AuthService.kt
@@ -4,6 +4,8 @@ import com.smashing.app.data.remote.dto.BaseResponse
 import com.smashing.app.data.remote.dto.auth.GetNickNameAvailableResponse
 import com.smashing.app.data.remote.dto.auth.PostKakaoLoginRequest
 import com.smashing.app.data.remote.dto.auth.PostKakaoLoginResponse
+import com.smashing.app.data.remote.dto.auth.PostOpenchatValidRequest
+import com.smashing.app.data.remote.dto.auth.PostOpenchatValidResponse
 import com.smashing.app.data.remote.dto.auth.PostSignUpRequest
 import com.smashing.app.data.remote.dto.auth.PostSignUpResponse
 import retrofit2.http.Body
@@ -26,4 +28,9 @@ interface AuthService {
     suspend fun getNickNameAvailable(
         @Query("nickname") nickname: String,
     ): BaseResponse<GetNickNameAvailableResponse>
+
+    @POST("/api/v1/users/openchat/validate")
+    suspend fun postOpenchatValid(
+        @Body request: PostOpenchatValidRequest,
+    ): BaseResponse<PostOpenchatValidResponse>
 }
