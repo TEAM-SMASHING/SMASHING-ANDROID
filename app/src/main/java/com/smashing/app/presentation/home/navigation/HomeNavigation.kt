c7a3538453d5e39b7b9f5b2696a5a1136e97466f
SHC <hcshin0717@naver.com>
2026-01-17 03:04:58 +0900

    [chore/#89] Region에 innerPadding 대신 systemBarsPadding 적용

--- app/src/main/java/com/smashing/app/presentation/home/navigation/HomeNavigation.kt
+++ app/src/main/java/com/smashing/app/presentation/home/navigation/HomeNavigation.kt
@@ -51,7 +51,7 @@ fun NavGraphBuilder.homeGraph(
             val savedStateHandle = backStackEntry.savedStateHandle
 
             RegionChangeRoute(
-                modifier = Modifier.padding(innerPadding),
+                modifier = Modifier,
                 navigateToRegion = navController::navigateToRegion,
                 navigateUp = navController::navigateUp,
                 regionResult = savedStateHandle.getRegionResult(),
