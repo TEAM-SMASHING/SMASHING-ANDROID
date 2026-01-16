0b3963bc7a6421ba15b8cefd3924083e229f8654
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-01-17 01:34:50 +0900

    [mod/#82] 임포트 및 시작화면 수정

--- app/src/main/java/com/smashing/app/presentation/main/MainAppState.kt
+++ app/src/main/java/com/smashing/app/presentation/main/MainAppState.kt
@@ -9,12 +9,11 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
 import com.smashing.app.core.extension.stateInWhileSubscribed
-import com.smashing.app.presentation.home.navigation.Home
 import com.smashing.app.presentation.home.navigation.navigateToHome
+import com.smashing.app.presentation.login.navigation.Login
 import com.smashing.app.presentation.matching.navigation.navigateToMatching
 import com.smashing.app.presentation.profile.navigation.navigateToProfile
 import com.smashing.app.presentation.search.navigation.navigateToSearch
-import com.smashing.app.presentation.signup.navigation.SignUp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -26,7 +25,7 @@ class MainAppState(
     val navController: NavHostController,
     coroutineScope: CoroutineScope,
 ) {
-    val startDestination = SignUp("dd")
+    val startDestination = Login
 
     private val currentDestination = navController.currentBackStackEntryFlow
         .map { it.destination }
