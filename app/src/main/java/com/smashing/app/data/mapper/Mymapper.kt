2a35291a0bb0c0e81d63efbb3de26f437f897edc
oilbeaneda <20221444@sungshin.ac.kr>
2026-01-20 08:38:55 +0900

    [feat/#119] 내 리뷰 조회 dtp, mapper

--- app/src/main/java/com/smashing/app/data/mapper/Mymapper.kt
+++ app/src/main/java/com/smashing/app/data/mapper/Mymapper.kt
@@ -1,10 +1,15 @@
 package com.smashing.app.data.mapper
 
+import com.smashing.app.data.model.cursor.Cursor
+import com.smashing.app.data.model.cursor.CursorPage
 import com.smashing.app.data.model.profile.MyPageInfo
 import com.smashing.app.data.model.profile.ProfileInfo
 import com.smashing.app.data.model.profile.SportProfile
+import com.smashing.app.data.model.review.GameReview
 import com.smashing.app.data.remote.dto.profile.my.AllProfileDto
 import com.smashing.app.data.remote.dto.profile.my.MyPageData
+import com.smashing.app.data.remote.dto.profile.my.MyProfileReviewListData
+import com.smashing.app.data.remote.dto.profile.my.MyReviewsDto
 import com.smashing.app.data.type.GenderType
 import com.smashing.app.data.type.SportType
 import com.smashing.app.data.type.SportType.Companion.findSportTypeToSportName
@@ -41,5 +46,27 @@ fun MyPageData.toMyPageInfo(): MyPageInfo {
     return MyPageInfo(
         profileInfo = this.toProfileInfo(),
         sportProfiles = this.allProfiles.map { it.toSportProfile() }
+    )
+}
+
+
+
+private fun MyProfileReviewListData.toGameReviewPage(): CursorPage<GameReview> {
+    return CursorPage(
+        items = this.results.map { it.toGameReview() },
+        cursor = Cursor(
+            snapshotAt = this.snapshotAt,
+            nextCursor = this.nextCursor,
+            hasNext = this.hasNext
+        )
+    )
+}
+
+private fun MyReviewsDto.toGameReview(): GameReview {
+    return GameReview(
+        gameReviewId = this.gameReviewId,
+        opponentNickname = this.nickname,
+        createdAt = this.createdAt,
+        content = this.content
     )
 }
