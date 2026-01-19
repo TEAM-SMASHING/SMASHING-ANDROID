6d4f16465a81f19bd1a01230c84e159448baa99d
oilbeaneda <20221444@sungshin.ac.kr>
2026-01-20 07:33:28 +0900

    [feat/#119]module

---
+++ app/src/main/java/com/smashing/app/data/di/profile/myprofile/MyDataSourceModule.kt
@@ -0,0 +1,19 @@
+package com.smashing.app.data.di.profile.myprofile
+
+import com.smashing.app.data.remote.datasource.api.MyRemoteDataSource
+import com.smashing.app.data.remote.datasource.impl.MyRemoteDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class MyDataSourceModule {
+    @Binds
+    @Singleton
+    protected abstract fun bindMyRemoteDataSource(
+        impl: MyRemoteDataSourceImpl
+    ): MyRemoteDataSource
+}
