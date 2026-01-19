a9b7280bee500e4f1e1a4eb9194dcfbd8c688f1f
oilbeaneda <20221444@sungshin.ac.kr>
2026-01-20 08:44:22 +0900

    [feat/#119]Service 구현

--- app/src/main/java/com/smashing/app/data/remote/service/MyService.kt
+++ app/src/main/java/com/smashing/app/data/remote/service/MyService.kt
@@ -2,9 +2,17 @@ package com.smashing.app.data.remote.service
 
 import com.smashing.app.data.remote.dto.BaseResponse
 import com.smashing.app.data.remote.dto.profile.my.MyPageData
+import com.smashing.app.data.remote.dto.profile.my.MyProfileReviewListData
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MyService {
     @GET("/api/v1/users/me/profiles")
     suspend fun getMyProfile(): BaseResponse<MyPageData>
+
+    @GET("/api/v1/users/me/reviews/recent?cursor={cursor?}&size={size?}")
+    suspend fun getMyGameReviews(
+        @Query("cursor") cursor: String? = null,
+        @Query("size") size: Int? = 50,
+    ): BaseResponse<MyProfileReviewListData>
 }
