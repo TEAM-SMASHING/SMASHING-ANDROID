591720136650ccd5b6f92b665d2be5590b4d6cb9
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-01-21 03:53:42 +0900

    [feat/#133] repository 추가

--- app/src/main/java/com/smashing/app/data/repository/api/UserRepository.kt
+++ app/src/main/java/com/smashing/app/data/repository/api/UserRepository.kt
@@ -1,8 +1,15 @@
 package com.smashing.app.data.repository.api
 
+import com.smashing.app.data.model.game.GameSubmission
+import com.smashing.app.data.model.profile.ProfileInfo
+
 interface UserRepository {
     suspend fun getUserId(): String?
     suspend fun getUserNickname(): String?
     suspend fun setUserInfo(userId: String, userNickname: String)
     suspend fun clearUserInfo()
+    suspend fun getUserInfoDetail(
+        userId: String,
+        sportCode: String?,
+    ): Result<ProfileInfo>
 }
--- app/src/main/java/com/smashing/app/data/repository/impl/UserRepositoryImpl.kt
+++ app/src/main/java/com/smashing/app/data/repository/impl/UserRepositoryImpl.kt
@@ -1,11 +1,18 @@
 package com.smashing.app.data.repository.impl
 
+import com.smashing.app.core.util.suspendRunCatching
 import com.smashing.app.data.local.datasource.api.LocalUserDataSource
+import com.smashing.app.data.mapper.toRankingData
+import com.smashing.app.data.mapper.user.toProfileInfo
+import com.smashing.app.data.model.profile.ProfileInfo
+import com.smashing.app.data.remote.datasource.api.UserRemoteDataSource
+import com.smashing.app.data.remote.dto.requireData
 import com.smashing.app.data.repository.api.UserRepository
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
     private val localUserDataSource: LocalUserDataSource,
+    private val userRemoteDataSource: UserRemoteDataSource,
 ) : UserRepository {
     override suspend fun getUserId(): String? =
         localUserDataSource.getUserId()
@@ -18,4 +25,13 @@ class UserRepositoryImpl @Inject constructor(
 
     override suspend fun clearUserInfo() =
         localUserDataSource.clearUserInfo()
+
+    override suspend fun getUserInfoDetail(
+        userId: String,
+        sportCode: String?
+    ): Result<ProfileInfo> =
+        suspendRunCatching{
+            userRemoteDataSource.getUserInfoDetail().requireData().toProfileInfo()
+        }
+
 }
