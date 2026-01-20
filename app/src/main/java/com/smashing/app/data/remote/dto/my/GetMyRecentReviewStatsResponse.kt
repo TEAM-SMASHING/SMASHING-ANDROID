706f85dd6f4fb321534f35388c71b1bb01e0a0b3
oilbeaneda <20221444@sungshin.ac.kr>
2026-01-21 08:37:46 +0900

    [feat/#140]dto 생성

---
+++ app/src/main/java/com/smashing/app/data/remote/dto/my/GetMyRecentReviewStatsResponse.kt
@@ -0,0 +1,34 @@
+package com.smashing.app.data.remote.dto.my
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GetMyRecentReviewStatsResponse(
+    @SerialName("ratingCounts")
+    val ratingCounts: RatingCounts,
+    @SerialName("tagCounts")
+    val tagCounts: TagCounts,
+) {
+    @Serializable
+    data class RatingCounts(
+        @SerialName("best")
+        val best: Long,
+        @SerialName("good")
+        val good: Long,
+        @SerialName("bad")
+        val bad: Long,
+    )
+
+    @Serializable
+    data class TagCounts(
+        @SerialName("goodManner")
+        val goodManner: Long,
+        @SerialName("onTime")
+        val onTime: Long,
+        @SerialName("fairPlay")
+        val fairPlay: Long,
+        @SerialName("fastResponse")
+        val fastResponse: Long,
+    )
+}
