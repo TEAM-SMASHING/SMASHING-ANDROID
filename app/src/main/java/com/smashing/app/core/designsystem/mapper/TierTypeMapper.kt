34bc0b1c1afa9232af0462906ec75122355efe97
SHC <hcshin0717@naver.com>
2026-01-21 00:51:43 +0900

    [chore/#136] tier별 이미지 지정

--- app/src/main/java/com/smashing/app/core/designsystem/mapper/TierTypeMapper.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/mapper/TierTypeMapper.kt
@@ -6,21 +6,21 @@ import com.smashing.app.data.type.TierType
 
 @DrawableRes
 fun TierType.img() = when(this){
-    TierType.IRON -> R.drawable.img_tier_dummy
+    TierType.IRON -> R.drawable.img_tier_sliver
     TierType.BRONZE_3,
     TierType.BRONZE_2,
-    TierType.BRONZE_1 -> R.drawable.img_tier_dummy
+    TierType.BRONZE_1 -> R.drawable.img_tier_bronze
     TierType.SILVER_3,
     TierType.SILVER_2,
-    TierType.SILVER_1 -> R.drawable.img_tier_dummy
+    TierType.SILVER_1 -> R.drawable.img_tier_sliver
     TierType.GOLD_3,
     TierType.GOLD_2,
-    TierType.GOLD_1 -> R.drawable.img_tier_dummy
+    TierType.GOLD_1 -> R.drawable.img_tier_gold
     TierType.PLATINUM_3,
     TierType.PLATINUM_2,
-    TierType.PLATINUM_1 -> R.drawable.img_tier_dummy
+    TierType.PLATINUM_1 -> R.drawable.img_tier_platinum
     TierType.DIAMOND_3,
     TierType.DIAMOND_2,
-    TierType.DIAMOND_1 -> R.drawable.img_tier_dummy
-    TierType.CHALLENGER -> R.drawable.img_tier_dummy
+    TierType.DIAMOND_1 -> R.drawable.img_tier_diamond
+    TierType.CHALLENGER -> R.drawable.img_tier_challenger
 }
