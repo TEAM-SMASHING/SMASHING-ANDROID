4ace286cb5a321f4ab95564c0e181f7c71c07d93
seungjun <jjw5741@naver.com>
2026-01-20 18:32:38 +0900

    [add/#127] 알림 목록 조회 api dto 정의

---
+++ app/src/main/java/com/smashing/app/data/remote/dto/notification/NotificationSummaryResponse.kt
@@ -0,0 +1,29 @@
+package com.smashing.app.data.remote.dto.notification
+
+import com.smashing.app.data.type.NotificationType
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class NotificationSummaryResponse(
+    @SerialName("notificationId")
+    val notificationId: String,
+    @SerialName("type")
+    val notificationType: NotificationType,
+    @SerialName("title")
+    val title: String,
+    @SerialName("content")
+    val content: String,
+    @SerialName("linkUrl")
+    val linkUrl: String,
+    @SerialName("isRead")
+    val isRead: Boolean,
+    @SerialName("createdAt")
+    val createdAt: String,
+    @SerialName("senderNickname")
+    val senderNickName: String,
+    @SerialName("receiverProfileId")
+    val receiverProfileId: String,
+    @SerialName("receiverSportId")
+    val receiverSportId: Long,
+)
