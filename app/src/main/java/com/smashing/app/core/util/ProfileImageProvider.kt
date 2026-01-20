f268b23729ef74a61326d1cad4ceb0222fc5ac75
SHC <hcshin0717@naver.com>
2026-01-21 01:07:46 +0900

    [chore/#136] profile img provider 수정

--- app/src/main/java/com/smashing/app/core/util/ProfileImageProvider.kt
+++ app/src/main/java/com/smashing/app/core/util/ProfileImageProvider.kt
@@ -1,24 +1,33 @@
 package com.smashing.app.core.util
 
-import kotlin.math.absoluteValue
+import com.smashing.app.R
+import com.smashing.app.R.drawable.img_profile_01
+import com.smashing.app.R.drawable.img_profile_02
+import com.smashing.app.R.drawable.img_profile_03
+import com.smashing.app.R.drawable.img_profile_04
+import com.smashing.app.R.drawable.img_profile_05
+import com.smashing.app.R.drawable.img_profile_06
+import com.smashing.app.R.drawable.img_profile_07
 
 object ProfileImageProvider {
-    // TODO 추후 프로필 이미지 변경 예정
     private val TEMP_PROFILE_IMAGE_URLS = listOf(
-        "https://picsum.photos/seed/1/100",
-        "https://picsum.photos/seed/2/100",
-        "https://picsum.photos/seed/3/100",
-        "https://picsum.photos/seed/4/100",
+        img_profile_01,
+        img_profile_02,
+        img_profile_03,
+        img_profile_04,
+        img_profile_05,
+        img_profile_06,
+        img_profile_07,
     )
 
     /**
      * userId를 기반으로 고정된 임시 URL을 반환합니다.
-     * @param userId 사용자 식별자 (null일 경우 기본 이미지 반환)
+     * @param nickname 사용자 식별자 (null일 경우 기본 이미지 반환)
      */
-    fun getTempUrl(userId: String?): String {
-        if (userId.isNullOrBlank()) return TEMP_PROFILE_IMAGE_URLS.first()
+    fun getTempImg(nickname: String?): Int {
+        if (nickname.isNullOrBlank()) return TEMP_PROFILE_IMAGE_URLS.first()
 
-        val index = userId.hashCode().absoluteValue % TEMP_PROFILE_IMAGE_URLS.size
+        val index = nickname.fold(0) { acc, char -> acc + char.code } % TEMP_PROFILE_IMAGE_URLS.size
         return TEMP_PROFILE_IMAGE_URLS[index]
     }
 }
