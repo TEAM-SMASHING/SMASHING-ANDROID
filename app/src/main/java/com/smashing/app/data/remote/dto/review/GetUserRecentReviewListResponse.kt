a265b69a1330e5e7fc06df66e7ff18510252df9b
oilbeaneda <20221444@sungshin.ac.kr>
2026-01-21 09:15:51 +0900

    [mod/#140] dto 분리

--- app/src/main/java/com/smashing/app/data/remote/dto/review/GetUserRecentReviewListResponse.kt
+++ app/src/main/java/com/smashing/app/data/remote/dto/review/GetUserRecentReviewListResponse.kt
@@ -14,33 +14,3 @@ data class GetUserRecentReviewListResponse(
     @SerialName("content")
     val content: String?,
 )
-
-@Serializable
-data class GetMyRecentReviewStatsResponse(
-    @SerialName("ratingCounts")
-    val ratingCounts: RatingCounts,
-    @SerialName("tagCounts")
-    val tagCounts: TagCounts,
-) {
-    @Serializable
-    data class RatingCounts(
-        @SerialName("best")
-        val best: Long,
-        @SerialName("good")
-        val good: Long,
-        @SerialName("bad")
-        val bad: Long,
-    )
-
-    @Serializable
-    data class TagCounts(
-        @SerialName("goodManner")
-        val goodManner: Long,
-        @SerialName("onTime")
-        val onTime: Long,
-        @SerialName("fairPlay")
-        val fairPlay: Long,
-        @SerialName("fastResponse")
-        val fastResponse: Long,
-    )
-}
