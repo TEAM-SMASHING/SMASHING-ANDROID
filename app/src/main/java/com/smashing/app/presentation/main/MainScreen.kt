d3e2ec6aa9364fee4c289bc862a264c2c2c814d7
seungjun <jjw5741@naver.com>
2026-01-22 02:05:33 +0900

    [feat/#162] Toast 구현

--- app/src/main/java/com/smashing/app/presentation/main/MainScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/main/MainScreen.kt
@@ -2,20 +2,28 @@ package com.smashing.app.presentation.main
 
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.navOptions
+import com.smashing.app.core.designsystem.component.toast.LocalToastTrigger
+import com.smashing.app.core.designsystem.component.toast.SmashingToast
 import com.smashing.app.core.designsystem.theme.SmashingTheme
 import com.smashing.app.presentation.addsports.navigation.addSportsGraph
-import com.smashing.app.presentation.addsports.navigation.navigateToAddSports
 import com.smashing.app.presentation.confirmreview.navigation.confirmReviewGraph
-import com.smashing.app.presentation.confirmreview.navigation.navigateToConfirmReview
 import com.smashing.app.presentation.home.navigation.homeGraph
 import com.smashing.app.presentation.home.navigation.navigateToHome
 import com.smashing.app.presentation.login.navigation.Login
@@ -25,7 +33,6 @@ import com.smashing.app.presentation.matching.navigation.Matching
 import com.smashing.app.presentation.matching.navigation.matchingGraph
 import com.smashing.app.presentation.matching.navigation.navigateToMatching
 import com.smashing.app.presentation.notice.navigation.noticeGraph
-import com.smashing.app.presentation.profile.navigation.navigateToReview
 import com.smashing.app.presentation.profile.navigation.profileGraph
 import com.smashing.app.presentation.ranking.navigation.rankingGraph
 import com.smashing.app.presentation.region.navigation.navigateToRegion
@@ -38,6 +45,11 @@ import com.smashing.app.presentation.write.navigation.navigateToConfirm
 import com.smashing.app.presentation.write.navigation.navigateToSubmit
 import com.smashing.app.presentation.write.navigation.writeGraph
 import kotlinx.collections.immutable.toImmutableList
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+
+private const val EXIT_MILLIS = 3000L
 
 @Composable
 fun MainScreen(
@@ -46,23 +58,66 @@ fun MainScreen(
     val isBottomBarVisible by appState.isBottomBarVisible.collectAsStateWithLifecycle()
     val currentTab by appState.currentTab.collectAsStateWithLifecycle()
 
-    Scaffold(
-        bottomBar = {
-            MainBottomBar(
-                isVisible = isBottomBarVisible,
-                tabs = MainTab.entries.toImmutableList(),
-                currentTab = currentTab,
-                onTabSelected = appState::navigate,
+    val snackBarHostState = remember { SnackbarHostState() }
+    val snackbarMutex = remember { Mutex() }
+
+    val coroutineScope = rememberCoroutineScope()
+    val onShowToast: (String) -> Unit = remember(coroutineScope, snackBarHostState, snackbarMutex) {
+        { message ->
+            coroutineScope.launch {
+                if (!snackbarMutex.tryLock()) return@launch
+                
+                try {
+                    launch {
+                        delay(EXIT_MILLIS)
+                        snackBarHostState.currentSnackbarData?.dismiss()
+                    }
+                    snackBarHostState.showSnackbar(
+                        message = message,
+                        withDismissAction = false,
+                    )
+                } finally {
+                    snackbarMutex.unlock()
+                }
+            }
+        }
+    }
+
+    CompositionLocalProvider(
+        LocalToastTrigger provides onShowToast,
+    ) {
+        Scaffold(
+            snackbarHost = {
+                val bottomPadding by animateDpAsState(
+                    targetValue = if (isBottomBarVisible) 20.dp else 46.dp,
+                    label = "snackbar_bottom_padding"
+                )
+                SnackbarHost(hostState = snackBarHostState) { data ->
+                    SmashingToast(
+                        text = data.visuals.message,
+                        modifier = Modifier
+                            .padding(bottom = bottomPadding)
+                            .padding(horizontal = 16.dp),
+                    )
+                }
+            },
+            bottomBar = {
+                MainBottomBar(
+                    isVisible = isBottomBarVisible,
+                    tabs = MainTab.entries.toImmutableList(),
+                    currentTab = currentTab,
+                    onTabSelected = appState::navigate,
+                )
+            },
+            containerColor = SmashingTheme.colors.bgCanvas,
+            modifier = Modifier
+                .fillMaxSize(),
+        ) { innerPadding ->
+            MainNavHost(
+                appState = appState,
+                innerPadding = innerPadding,
             )
-        },
-        containerColor = SmashingTheme.colors.bgCanvas,
-        modifier = Modifier
-            .fillMaxSize(),
-    ) { innerPadding ->
-        MainNavHost(
-            appState = appState,
-            innerPadding = innerPadding,
-        )
+        }
     }
 }
 
@@ -195,7 +250,7 @@ private fun MainNavHost(
         )
 
         confirmReviewGraph(
-            navigateUp =  appState.navController::navigateUp,
+            navigateUp = appState.navController::navigateUp,
         )
     }
 }
