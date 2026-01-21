a0c0db3d85a7f4ef252f5dfe69e7929beade7253
oilbeaneda <20221444@sungshin.ac.kr>
2026-01-21 23:54:15 +0900

    [mod/#161]후기작성 탑바 back 추가

--- app/src/main/java/com/smashing/app/presentation/write/confirm/ConfirmReviewScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/write/confirm/ConfirmReviewScreen.kt
@@ -95,6 +95,7 @@ private fun ConfirmReviewScreen(
         SmashingDefaultTopBar(
             title = "후기 작성",
             topBarType = TopBarType.BACK,
+            onClick = onBackClick,
         )
 
         Column(
