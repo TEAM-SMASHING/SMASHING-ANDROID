cca9f6ca364b9233b940e4dbd6537e87ab053106
oilbeaneda <20221444@sungshin.ac.kr>
2026-01-21 23:27:34 +0900

    [mod/#161]divider 마지막 아이템에는 없어야함

--- app/src/main/java/com/smashing/app/presentation/profile/component/ProfileReviewCard.kt
+++ app/src/main/java/com/smashing/app/presentation/profile/component/ProfileReviewCard.kt
@@ -37,7 +37,6 @@ import com.smashing.app.presentation.profile.myprofile.MyProfileContract
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
-private const val REVIEW_ITEM_COUNT = 3
 
 @Composable
 fun ReviewCard(
@@ -121,7 +120,7 @@ fun ReviewCard(
                         review = review,
                     )
 
-                    if (index < REVIEW_ITEM_COUNT - 1) {
+                    if (index < reviews.size - 1) {
                         HorizontalDivider(
                             modifier = Modifier.padding(vertical = 12.dp),
                             thickness = 1.dp,
