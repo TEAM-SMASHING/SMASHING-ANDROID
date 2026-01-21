9b5f7c972437fc3c7ef5e170e01815646d9e4c0d
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-01-21 23:04:49 +0900

    [mod/#162] 자음만 작성시 버튼 비활성화

--- app/src/main/java/com/smashing/app/core/util/TextInputValidator.kt
+++ app/src/main/java/com/smashing/app/core/util/TextInputValidator.kt
@@ -2,14 +2,18 @@ package com.smashing.app.core.util
 
 object TextInputValidator {
 
-    private val textRegex = "^[가-힣a-zA-Z0-9]*$".toRegex()
-
+    private val textSpecialRegex = "^[ㄱ-ㅎ가-힣a-zA-Z0-9]*$".toRegex()
+    private val hasTextRegex = "[ㄱ-ㅎ]".toRegex()
     /**
      * 한글, 영어, 숫자만 포함되어 있는지 검증하는 함수입니다.
      * @param text 검증할 텍스트
      */
 
-    fun isTextInputValid(text: String): Boolean {
-        return text.matches(textRegex)
+    fun isTextInputSpecialValid(text: String): Boolean {
+        return text.matches(textSpecialRegex)
+    }
+
+    fun isTextFinished(text: String): Boolean {
+        return !hasTextRegex.containsMatchIn(text)
     }
 }
