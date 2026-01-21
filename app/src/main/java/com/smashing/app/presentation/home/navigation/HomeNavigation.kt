72addddc2dd3a0eef327dab50797083121689823
SHC <hcshin0717@naver.com>
2026-01-21 15:10:12 +0900

    [chore/#151] Home 스포츠 파라미터 변경

--- app/src/main/java/com/smashing/app/presentation/home/navigation/HomeNavigation.kt
+++ app/src/main/java/com/smashing/app/presentation/home/navigation/HomeNavigation.kt
@@ -41,7 +41,7 @@ fun NavGraphBuilder.homeGraph(
                 navigateToRegionChange = navController::navigateToRegionChange,
                 navigateToRanking = navController::navigateToRanking,
                 navigateToTierInfo = { tierInfoStyle, sportType ->
-                    navController.navigateToTierInfo(tierName = tierInfoStyle.name, sportName = sportType.code)
+                    navController.navigateToTierInfo(tierName = tierInfoStyle.name, sportName = sportType.sportName)
                 },
             )
         }
