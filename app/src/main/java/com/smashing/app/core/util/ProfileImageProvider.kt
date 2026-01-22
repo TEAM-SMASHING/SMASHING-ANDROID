7cc67d34a1bd6241eca7e0fbd870fe348f369980
SHC <hcshin0717@naver.com>
2026-01-23 05:48:06 +0900

    [add/#193] import 추가

--- app/src/main/java/com/smashing/app/core/util/ProfileImageProvider.kt
+++ app/src/main/java/com/smashing/app/core/util/ProfileImageProvider.kt
@@ -1,5 +1,6 @@
 package com.smashing.app.core.util
 
+import com.smashing.app.R.drawable.img_default_profile
 import com.smashing.app.R.drawable.img_profile01_2
 import com.smashing.app.R.drawable.img_profile02_2
 import com.smashing.app.R.drawable.img_profile03_2
