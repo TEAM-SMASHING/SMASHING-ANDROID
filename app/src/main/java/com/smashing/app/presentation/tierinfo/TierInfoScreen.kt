1a4852a14c4e9326a99c6bafaa2e4059d8cb8f2a
oilbeaneda <20221444@sungshin.ac.kr>
2026-01-22 09:32:51 +0900

    [mod/#178]티어인포 텍스트 스타일 수정 및 중앙정렬

--- app/src/main/java/com/smashing/app/presentation/tierinfo/TierInfoScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/tierinfo/TierInfoScreen.kt
@@ -22,7 +22,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -30,12 +30,9 @@ import com.smashing.app.core.designsystem.component.appicon.AppIcon
 import com.smashing.app.core.designsystem.component.chip.SmashingChip
 import com.smashing.app.core.designsystem.component.topbar.SmashingDefaultTopBar
 import com.smashing.app.core.designsystem.style.ChipStyle
-import com.smashing.app.core.designsystem.style.TopBarType
-import com.smashing.app.core.designsystem.theme.SmashingAndroidTheme
-import com.smashing.app.core.designsystem.theme.SmashingTheme
-import com.smashing.app.data.type.TierType
 import com.smashing.app.core.designsystem.style.TierInfoStyle
-import com.smashing.app.core.designsystem.style.toTierInfoStyle
+import com.smashing.app.core.designsystem.style.TopBarType
+import com.smashing.app.core.designsystem.theme.SmashingTheme
 
 @Composable
 fun TierInfoRoute(
@@ -89,6 +86,7 @@ private fun TierInfoScreen(
                 text = uiState.selectedTierInfoStyle.tierName,
                 style = SmashingTheme.typography.xl.semibold20,
                 color = uiState.selectedTierInfoStyle.getTxtColor(),
+                textAlign = TextAlign.Center,
             )
             Spacer(modifier = Modifier.height(12.dp))
             Row(
@@ -146,7 +144,7 @@ private fun TierInfoScreen(
             if (uiState.selectedTierInfoStyle != TierInfoStyle.CHALLENGER && uiState.selectedTierInfoStyle != TierInfoStyle.IRON) {
                 Text(
                     text = "승급을 위해 아래의 기술들을 연마해보세요",
-                    style = SmashingTheme.typography.sm.semibold14,
+                    style = SmashingTheme.typography.md.semibold16,
                     color = SmashingTheme.colors.txtPrimary,
                 )
 
@@ -209,6 +207,7 @@ private fun TierTag(
             .padding(
                 8.dp,
             ),
+        textAlign = TextAlign.Center,
     )
 }
 
@@ -242,23 +241,6 @@ private fun CommentTag(
             text = comment ?: "",
             style = SmashingTheme.typography.xs.medium12,
             color = SmashingTheme.colors.txtSecondary,
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun TierInfoScreenPreview() {
-    SmashingAndroidTheme {
-        val dummyState = TierInfoContract.State(
-            selectedTierInfoStyle = TierType.GOLD_1.toTierInfoStyle(),
-        )
-
-        TierInfoScreen(
-            uiState = dummyState,
-            modifier = Modifier,
-            onClick = {},
-            onBack = {},
         )
     }
 }
