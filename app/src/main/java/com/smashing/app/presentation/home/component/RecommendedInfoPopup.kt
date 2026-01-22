62ae3d823a4133df16426caccdcbd1a28be7b90e
SHC <hcshin0717@naver.com>
2026-01-23 00:33:45 +0900

    [del/#195] unused import 제거

--- app/src/main/java/com/smashing/app/presentation/home/component/RecommendedInfoPopup.kt
+++ app/src/main/java/com/smashing/app/presentation/home/component/RecommendedInfoPopup.kt
@@ -1,19 +1,15 @@
 package com.smashing.app.presentation.home.component
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.unit.dp
 import com.smashing.app.core.designsystem.theme.SmashingTheme
 
--- app/src/main/java/com/smashing/app/presentation/search/searchmain/component/SearchTopBar.kt
+++ app/src/main/java/com/smashing/app/presentation/search/searchmain/component/SearchTopBar.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,7 +14,6 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.smashing.app.R.drawable.ic_search_lg
-import com.smashing.app.R.drawable.ic_bell
 import com.smashing.app.core.designsystem.component.dropdown.RegionDropdown
 import com.smashing.app.core.designsystem.theme.SmashingAndroidTheme
 import com.smashing.app.core.designsystem.theme.SmashingTheme.colors
