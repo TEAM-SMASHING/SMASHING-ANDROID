e31253fc7b3818f9d16cb0a1103698f621263409
seungjun <jjw5741@naver.com>
2026-01-22 18:27:36 +0900

    [add/#6] local token datasource flow 기반 응답 값 추가

--- app/src/main/java/com/smashing/app/data/local/datasource/api/LocalTokenDataSource.kt
+++ app/src/main/java/com/smashing/app/data/local/datasource/api/LocalTokenDataSource.kt
@@ -1,6 +1,9 @@
 package com.smashing.app.data.local.datasource.api
 
+import kotlinx.coroutines.flow.Flow
+
 interface LocalTokenDataSource {
+    val accessTokenFlow: Flow<String?>
     suspend fun getAccessToken(): String?
     suspend fun getRefreshToken(): String?
     suspend fun setTokens(accessToken: String, refreshToken: String)
