664a896d01110427aaffb0fc1ab48ccb5a52c17c
seungjun <jjw5741@naver.com>
2026-02-07 15:36:52 +0900

    [del/#224] 내장 kotlin 사용으로 인한 kotlin-android 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,7 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.hilt)
@@ -75,12 +73,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-        }
     }
 
     buildFeatures {
