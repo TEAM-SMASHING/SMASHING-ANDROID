a2acd4be3cca1caf9c92fc06281c0bcd6fa198fa
seungjun <jjw5741@naver.com>
2026-02-07 14:43:24 +0900

    [refactor/#224] r8 설정 및 rule setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,8 @@ android {
 
         release {
             isDebuggable = false
-            isMinifyEnabled = false // TODO: enable in release -isShrinkResources = true
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
