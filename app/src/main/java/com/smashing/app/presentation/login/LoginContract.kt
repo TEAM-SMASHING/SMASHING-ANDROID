1c9963c05b9a8ad15bacd2ebd87aeb5c3c0af7ce
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-02-13 00:35:44 +0900

    [del/#228] loginUiState 삭제

--- app/src/main/java/com/smashing/app/presentation/login/LoginContract.kt
+++ app/src/main/java/com/smashing/app/presentation/login/LoginContract.kt
@@ -6,15 +6,3 @@ interface LoginContract {
         data class NavigateToSignUp(val kakaoId: String): SideEffect
     }
 }
-
-sealed interface LoginUiState {
-    object Idle : LoginUiState
-
-    object Loading : LoginUiState
-
-    object Success : LoginUiState
-
-    data class Failure(
-        val msg: String,
-    ) : LoginUiState
-}
