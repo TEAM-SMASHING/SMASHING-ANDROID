f31eb3926870b96c80b3cadb938649dd27c17ccb
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-02-13 00:05:49 +0900

    [feat/#228] LoginUiState 추가

--- app/src/main/java/com/smashing/app/presentation/login/LoginContract.kt
+++ app/src/main/java/com/smashing/app/presentation/login/LoginContract.kt
@@ -6,3 +6,15 @@ interface LoginContract {
         data class NavigateToSignUp(val kakaoId: String): SideEffect
     }
 }
+
+sealed interface LoginUiState {
+    object Idle : LoginUiState
+
+    object Loading : LoginUiState
+
+    object Success : LoginUiState
+
+    data class Failure(
+        val msg: String,
+    ) : LoginUiState
+}
