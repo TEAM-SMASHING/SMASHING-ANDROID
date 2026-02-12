ab4b5a7795539292d675463ec7afeda994c5b7ff
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-02-13 00:37:03 +0900

    [mod/#228] SocialLoginManager에서 성공/실패 관리

--- app/src/main/java/com/smashing/app/presentation/login/SocialLoginManager.kt
+++ app/src/main/java/com/smashing/app/presentation/login/SocialLoginManager.kt
@@ -6,37 +6,56 @@ import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 import com.smashing.app.core.util.suspendRunCatching
+import kotlinx.coroutines.suspendCancellableCoroutine
 import timber.log.Timber
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 class SocialLoginManager(
-    val context: Context
+    val context: Context,
+    val viewModel: LoginViewModel
 ) {
-
-    private fun getKakaoAccessToken() {
-        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-            if (error != null) {
-                Timber.e("카카오계정으로 로그인 실패 ${error}")
-            } else if (token != null) {
-                Timber.i("카카오톡으로 로그인 성공 ${token.accessToken}")
-            }
+    suspend fun loginKakao(): Result<String> =
+        suspendRunCatching {
+            val accessToken = getKakaoAccessToken()
+            accessToken
+        }.onSuccess { token ->
+            viewModel.postKakaoLogin(token)
+        }.onFailure { error ->
+            Timber.tag("KakaoLogin").e("로그인 실패 : $error")
         }
 
-        if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
-            UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+    suspend fun getKakaoAccessToken(): String =
+        suspendCancellableCoroutine { continuation ->
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
                 if (error != null) {
-                    Timber.e("카카오톡으로 로그인 실패 ${error}")
-
-                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                        return@loginWithKakaoTalk
-                    }
-
-                    UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                    continuation.resumeWithException(error)
+                    Timber.e("카카오계정으로 로그인 실패 ${error}")
                 } else if (token != null) {
+                    continuation.resume(token.accessToken)
                     Timber.i("카카오톡으로 로그인 성공 ${token.accessToken}")
                 }
             }
-        } else {
-            UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+                UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+                    if (error != null) {
+                        continuation.resumeWithException(error)
+                        Timber.e("카카오톡으로 로그인 실패 ${error}")
+
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            continuation.resumeWithException(error)
+                            return@loginWithKakaoTalk
+                        }
+
+                        UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                    } else if (token != null) {
+                        continuation.resume(token.accessToken)
+                        Timber.i("카카오톡으로 로그인 성공 ${token.accessToken}")
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+            }
         }
-    }
 }
