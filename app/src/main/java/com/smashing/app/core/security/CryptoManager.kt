1d7ec65727bbf4f536b12af56b68229001b01240
SHC <hcshin0717@naver.com>
2026-02-16 20:42:05 +0900

    [feat/#229] Crypto Manager 구현

---
+++ app/src/main/java/com/smashing/app/core/security/CryptoManager.kt
@@ -0,0 +1,63 @@
+package com.smashing.app.core.security
+
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import java.security.KeyStore
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.GCMParameterSpec
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CryptoManager @Inject constructor(): CryptoInterface {
+
+    private val keyAlias = "UserAccessToken"
+    private val keystoreProvider = "AndroidKeyStore"
+
+    override suspend fun encrypt(data: List<String>): EncryptedResult {
+        val secretKey = getOrCreateSecretKey()
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+
+        val iv = cipher.iv
+        val encrypted = cipher.doFinal(data.joinToString(" ").toByteArray())
+
+        return EncryptedResult(
+            ciphertext = encrypted,
+            iv = iv
+        )
+    }
+
+    override suspend fun decrypt(encryptedData: ByteArray, iv: ByteArray): String {
+        if (encryptedData.isEmpty() && iv.isEmpty()){
+            return ""
+        }
+        val secretKey = getOrCreateSecretKey()
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        val spec = GCMParameterSpec(128, iv)
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, spec)
+
+        val decrypted = cipher.doFinal(encryptedData)
+        return String(decrypted)
+    }
+
+    private fun getOrCreateSecretKey(): SecretKey {
+        val keyStore = KeyStore.getInstance(keystoreProvider).apply { load(null) }
+        keyStore.getKey(keyAlias, null)?.let { return it as SecretKey }
+
+        val keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, keystoreProvider)
+        val keySpec = KeyGenParameterSpec.Builder(
+            keyAlias,
+            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+        ).run {
+            setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+            setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+            build()
+        }
+
+        keyGenerator.init(keySpec)
+        return keyGenerator.generateKey()
+    }
+}
