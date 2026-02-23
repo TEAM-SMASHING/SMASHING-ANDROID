5f1e7430b7d0743a48aaa811e07d43a57c55ee32
seungjun <jjw5741@naver.com>
2026-02-23 09:28:57 +0900

    [refactor/#225] signUpGraph navController 전달에 따른 변경

--- app/src/main/java/com/smashing/app/presentation/signup/navigation/SignUpNavigation.kt
+++ app/src/main/java/com/smashing/app/presentation/signup/navigation/SignUpNavigation.kt
@@ -1,12 +1,14 @@
 package com.smashing.app.presentation.signup.navigation
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.smashing.app.core.extension.clearBackStackOptions
+import com.smashing.app.presentation.home.navigation.navigateToHome
 import com.smashing.app.presentation.region.navigation.getRegionResult
+import com.smashing.app.presentation.region.navigation.navigateToRegion
 import com.smashing.app.presentation.region.navigation.removeRegionResult
 import com.smashing.app.presentation.signup.SignUpRoute
 import kotlinx.serialization.Serializable
@@ -17,20 +19,19 @@ fun NavController.navigateToSignUp(
 ) = navigate(SignUp(kakaoId), navOptions)
 
 fun NavGraphBuilder.signUpGraph(
-    navigateToRegion: () -> Unit,
-    navigateToHome: () -> Unit,
-    navigateUp: () -> Unit,
-    innerPadding: PaddingValues,
+    navController: NavController,
 ) {
-    composable<SignUp> {backStackEntry ->
+    composable<SignUp> { backStackEntry ->
         val savedStateHandle = backStackEntry.savedStateHandle
 
         SignUpRoute(
+            navigateUp = navController::navigateUp,
             regionResult = savedStateHandle.getRegionResult(),
-            navigateUp = navigateUp,
             onRegionResultConsumed = savedStateHandle::removeRegionResult,
-            navigateToRegion = navigateToRegion,
-            navigateToHome = navigateToHome,
+            navigateToRegion = navController::navigateToRegion,
+            navigateToHome = {
+                navController.navigateToHome(navOptions = clearBackStackOptions())
+            },
             modifier = Modifier,
         )
     }
