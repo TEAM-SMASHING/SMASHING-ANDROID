931290ea40f73db6f6510c1710cd2360198d106d
oilbeaneda <20221444@sungshin.ac.kr>
2026-03-05 21:57:44 +0900

    [mod/#248] systemBarsPadding추가

--- app/src/main/java/com/smashing/app/presentation/mypage/MypageScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/mypage/MypageScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
@@ -111,7 +112,8 @@ private fun MyPageScreen(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(color = colors.bgCanvas),
+            .background(color = colors.bgCanvas)
+            .systemBarsPadding(),
     ) {
 
         SmashingDefaultTopBar(
