7ffbb7aba1eb6982b7588482a177a69ece950a6c
oilbeaneda <20221444@sungshin.ac.kr>
2026-03-05 21:13:10 +0900

    [mod/#248]import 오류 변경

--- app/src/main/java/com/smashing/app/presentation/mypage/MypageScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/mypage/MypageScreen.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.kakao.sdk.v2.auth.BuildConfig
+import com.smashing.app.BuildConfig
 import com.smashing.app.R.drawable.ic_arrow_next
 import com.smashing.app.R.string.mypage
 import com.smashing.app.R.string.mypage_account_logout
