dee306f02976a4d9c598ddb1b984791b69d6898e
SHC <hcshin0717@naver.com>
2026-03-08 15:34:58 +0900

    [del/#246] unused import 제거

--- app/src/main/java/com/smashing/app/core/designsystem/component/topbar/SmashingDefaultTopBar.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/topbar/SmashingDefaultTopBar.kt
@@ -3,7 +3,6 @@ package com.smashing.app.core.designsystem.component.topbar
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
