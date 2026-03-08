63f352353818f4710404c69f425265a7b69f8aa8
SHC <hcshin0717@naver.com>
2026-03-08 21:19:00 +0900

    [feat/#259] withdraw시 제거되는 데이터 type 생성

---
+++ app/src/main/java/com/smashing/app/presentation/withdraw/type/WithdrawalDeletedData.kt
@@ -0,0 +1,11 @@
+package com.smashing.app.presentation.withdraw.type
+
+enum class WithdrawalDeletedData(
+    val text: String,
+) {
+    PROFILE_AND_ACCOUNT("프로필 및 계정 정보"),
+    MATCHING_AND_GAME_RECORDS("매칭 및 경기 기록"),
+    REVIEWS_AND_RATINGS("작성한 후기 및 평가"),
+    CHAT_HISTORY("채팅 내역"),
+    NOTIFICATIONS_AND_BLOCK_REPORT_HISTORY("알림 및 차단 / 신고 내역"),
+}
