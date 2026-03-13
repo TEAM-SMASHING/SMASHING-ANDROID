dd86d87b7cabd409fae78cbfff7ba6c97a03f2dd
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-03-14 02:04:32 +0900

    [mod/#264] SplashScreen 수정

--- app/src/main/java/com/smashing/app/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/splash/SplashScreen.kt
@@ -2,16 +2,19 @@ package com.smashing.app.presentation.splash
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.smashing.app.R.drawable.img_splash_logo
 import com.smashing.app.core.designsystem.theme.SmashingTheme.colors
 
@@ -22,6 +25,7 @@ fun SplashRoute(
     navigateToLogin: () -> Unit,
     navigateToHome: () -> Unit,
     modifier: Modifier = Modifier,
+    viewModel: SplashViewModel = hiltViewModel()
 ) {
 
     SplashScreen(
@@ -35,7 +39,10 @@ private fun SplashScreen(
 ) {
     Column(
         modifier = modifier
+            .fillMaxSize()
             .background(color = colors.bgCanvas),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ){
         Spacer(modifier = Modifier.weight(PADDING_RATIO))
 
@@ -44,8 +51,7 @@ private fun SplashScreen(
             contentDescription = null,
             modifier = Modifier
                 .size(260.dp)
-                .aspectRatio(1f)
-                .padding(horizontal = 50.dp),
+                .aspectRatio(1f),
         )
 
         Spacer(modifier = Modifier.weight(1f))
