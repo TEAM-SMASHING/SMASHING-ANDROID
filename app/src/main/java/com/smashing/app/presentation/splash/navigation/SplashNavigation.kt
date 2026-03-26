27d374d4007c41a7190ac3b11021c945ac252d3a
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-03-26 21:20:44 +0900

    [mod/#264] 로그인 화면 이동시 백스택 정리

--- app/src/main/java/com/smashing/app/presentation/splash/navigation/SplashNavigation.kt
+++ app/src/main/java/com/smashing/app/presentation/splash/navigation/SplashNavigation.kt
@@ -1,23 +1,25 @@
 package com.smashing.app.presentation.splash.navigation
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import androidx.navigation.navigation
 import com.smashing.app.core.common.navigation.Route
+import com.smashing.app.core.extension.clearBackStackNavOptions
 import com.smashing.app.presentation.home.navigation.navigateToHome
 import com.smashing.app.presentation.login.navigation.navigateToLogin
 import com.smashing.app.presentation.splash.SplashRoute
 import kotlinx.serialization.Serializable
 
 fun NavGraphBuilder.splashGraph(
-    innerPadding: PaddingValues,
     navController: NavController,
 ) {
     composable<Splash> {
         SplashRoute(
-            navigateToLogin = navController::navigateToLogin,
+            navigateToLogin = {
+                navController.navigateToLogin(
+                    navOptions = navController.clearBackStackNavOptions()
+                )
+            },
             navigateToHome = navController::navigateToHome,
         )
     }
