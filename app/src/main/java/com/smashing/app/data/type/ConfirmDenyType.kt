6df785729ae79056c6bd65a62020b7bb237e3b4c
seungjun <jjw5741@naver.com>
2026-03-27 00:53:29 +0900

    [fix/#284] ConfirmDenyType 상수명 변경 (WIN_LOSE_REVERSED -> WINNER_MISMATCH)

--- app/src/main/java/com/smashing/app/data/type/ConfirmDenyType.kt
+++ app/src/main/java/com/smashing/app/data/type/ConfirmDenyType.kt
@@ -1,7 +1,7 @@
 package com.smashing.app.data.type
 
 enum class ConfirmDenyType(val description: String) {
-    WIN_LOSE_REVERSED("승자가 잘못됐어요"),
+    WINNER_MISMATCH("승자가 잘못됐어요"),
     GAME_NOT_PLAYED_YET("아직 진행하지 않은 경기에요");
 
     companion object {
