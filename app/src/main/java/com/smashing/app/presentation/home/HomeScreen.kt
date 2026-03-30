daf7cb4b28fac3898c90794dfd400c90ebabf37b
seungjun <jjw5741@naver.com>
2026-03-30 18:47:17 +0900

    [chore/#298] CloseMatching 컴포넌트 미사용 파라미터(userId) 제거

--- app/src/main/java/com/smashing/app/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/smashing/app/presentation/home/HomeScreen.kt
@@ -300,7 +300,6 @@ private fun HomeScreen(
                     Spacer(modifier = Modifier.height(20.dp))
 
                     CloseMatching(
-                        myProfileId = uiState.activeMyProfile.myProfileInfo.profileId,
                         myNickname = uiState.activeMyProfile.nickname,
                         matchedUser = uiState.matchedUser,
                         onClick = { matching ->
