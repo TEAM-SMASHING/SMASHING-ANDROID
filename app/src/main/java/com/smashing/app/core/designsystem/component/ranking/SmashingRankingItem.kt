ee4e7e7011675827239c0a198b65ea88ab5a0879
SHC <hcshin0717@naver.com>
2026-04-04 23:16:08 +0900

    [fix/#305] 랭킹 아이템 todo 제거

--- app/src/main/java/com/smashing/app/core/designsystem/component/ranking/SmashingRankingItem.kt
+++ app/src/main/java/com/smashing/app/core/designsystem/component/ranking/SmashingRankingItem.kt
@@ -136,7 +136,6 @@ fun SmashingRankingItem(
 
         Spacer(modifier = Modifier.weight(1f))
 
-        //TODO 티어 뱃지 TierType 사용해 이미지 수정 예정
         Image(
             painter = painterResource(id = tier.img()),
             contentDescription = null,
